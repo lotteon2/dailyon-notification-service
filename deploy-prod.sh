807c1759682481a0e7ff11dd5d44cc5c3053053a
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-22 12:40:22 +0900

    O2F-746 [FEAT] 데이터 볼륨 마운트 추가

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -6,10 +6,24 @@ export IMAGE_TAG=${IMAGE_TAG}
 
 deployment_name="notification-service"
 service_name="notification-service"
+pv_name="notification-ebs-pv"
+pvc_name="notification-ebs-pvc"
 namespace="prod"
 
 # Update AWS EKS user config
 aws eks update-kubeconfig --region ap-northeast-2 --name $AWS_EKS_CLUSTER_NAME
+
+if kubectl get pv "${service_name}" -n "${namespace}" &> /dev/null; then
+  echo "PersistenceVolume is already exists"
+else
+  kubectl create -f ./pv-prod.yml
+fi
+
+if kubectl get pvc "${service_name}" -n "${namespace}" &> /dev/null; then
+  echo "PersistenceVolumeClaim is already exists"
+else
+  kubectl create -f ./pvc-prod.yml
+fi
 
 # Deploy kubernetes deployment resource
 echo "Apply new kubernetes deployment resources..."
