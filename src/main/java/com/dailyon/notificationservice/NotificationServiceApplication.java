006ea6212a6228e45e17193557ca7eb2b104318f
CessnaJ <eon8748@gmail.com>
2023-12-26 03:22:03 +0900

    O2F-966 [INIT] discoveryClient annotation 추가

--- src/main/java/com/dailyon/notificationservice/NotificationServiceApplication.java
+++ src/main/java/com/dailyon/notificationservice/NotificationServiceApplication.java
@@ -2,7 +2,9 @@ package com.dailyon.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NotificationServiceApplication {
 
