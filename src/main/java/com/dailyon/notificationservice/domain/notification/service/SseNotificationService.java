d310866ad8e295dab21689ce0d12e746e0dea3e7
CessnaJ <eon8748@gmail.com>
2023-12-26 19:26:48 +0900

    [FEAT] deprecated된 EmitterProcessor -> Sinks로 개선

--- src/main/java/com/dailyon/notificationservice/domain/notification/service/SseNotificationService.java
+++ src/main/java/com/dailyon/notificationservice/domain/notification/service/SseNotificationService.java
@@ -5,7 +5,8 @@ import com.dailyon.notificationservice.domain.notification.repository.Notificati
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.EmitterProcessor;
+import reactor.core.publisher.Sinks;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -16,38 +17,37 @@ import java.util.function.Consumer;
 @RequiredArgsConstructor
 public class SseNotificationService {
     private final NotificationTemplateRepository notificationRepository;
-    private final ConcurrentHashMap<Long, EmitterProcessor<ServerSentEvent<NotificationTemplate>>> userEmitters
+    private final ConcurrentHashMap<Long, Sinks.Many<ServerSentEvent<NotificationTemplate>>> userSinks
             = new ConcurrentHashMap<>();
 
     // 구독하기
     public Flux<ServerSentEvent<NotificationTemplate>> streamNotifications(Long memberId) {
-        EmitterProcessor<ServerSentEvent<NotificationTemplate>> emitter = EmitterProcessor.create();
-        userEmitters.put(memberId, emitter);
+        Sinks.Many<ServerSentEvent<NotificationTemplate>> sink = Sinks.many().multicast().onBackpressureBuffer();
+        userSinks.put(memberId, sink);
 
-        Consumer<Throwable> removeEmitterConsumer = e -> userEmitters.remove(memberId);
+        Consumer<Throwable> removeSinkConsumer = e -> userSinks.remove(memberId);
 
-        return emitter
-                .doOnCancel(() -> removeEmitterConsumer.accept(null))
-                .doOnError(removeEmitterConsumer);
+        return sink.asFlux()
+                .doOnCancel(() -> removeSinkConsumer.accept(null))
+                .doOnError(removeSinkConsumer);
     }
 
+    // TODO: AWS SQS 받으면 작성할 로직
     public void pushNotificationToMember(NotificationTemplate notification, Long memberId) {
-        EmitterProcessor<ServerSentEvent<NotificationTemplate>> emitter = userEmitters.get(memberId);
-        if (emitter != null && !emitter.isTerminated()) {
-            emitter.onNext(ServerSentEvent.builder(notification)
+        Sinks.Many<ServerSentEvent<NotificationTemplate>> sink = userSinks.get(memberId);
+        if (sink != null) {
+            sink.tryEmitNext(ServerSentEvent.builder(notification)
                     .event("notification-event")
                     .id(String.valueOf(notification.getId()))
-                    .build());
+                    .build())
+                    .orThrow();
         }
     }
 
-    // Imagine a method that is called by a Kafka listener just like in the original SseService.java
-    // Here we illustrate how it might look by just using a placeholder
-    // SQS 받으면 작성할 로직
+    // Kafka listener 대신 SQS를 쓰기로 결정했음.
+    // TODO: AWS SQS 받으면 작성할 로직
     public Mono<Void> onNotificationReceived(NotificationTemplate notification, Long memberId) {
         return Mono.fromRunnable(() -> pushNotificationToMember(notification, memberId))
                 .then(Mono.empty());
     }
-    
-
 }
