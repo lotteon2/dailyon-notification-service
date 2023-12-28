92d85e13eb726ae08ca024c0c66c7188ead7a6d3
CessnaJ <eon8748@gmail.com>
2023-12-28 16:03:55 +0900

    [FEAT] 안읽은 알림 개수받기 api 수정, postman test 완료 주석 추가

--- src/main/java/com/dailyon/notificationservice/domain/notification/api/NotificationApiController.java
+++ src/main/java/com/dailyon/notificationservice/domain/notification/api/NotificationApiController.java
@@ -1,6 +1,7 @@
 package com.dailyon.notificationservice.domain.notification.api;
 
 import com.dailyon.notificationservice.domain.notification.document.NotificationTemplate;
+import com.dailyon.notificationservice.domain.notification.dto.NotificationData;
 import com.dailyon.notificationservice.domain.notification.service.NotificationService;
 import com.dailyon.notificationservice.domain.notification.service.SseNotificationService;
 import lombok.RequiredArgsConstructor;
@@ -18,53 +19,53 @@ public class NotificationApiController {
     private final NotificationService notificationService;
     private final SseNotificationService sseNotificationService;
 
-    // 최근 알림 5개 받기
+    // 최근 unread 알림 5개 받기 - 테스트완료
     @GetMapping("")
-    public Flux<NotificationTemplate> getRecentNotifications(@RequestHeader Long memberId) {
+    public Flux<NotificationData> getRecentNotifications(@RequestHeader Long memberId) {
         return notificationService.getRecentNotifications(memberId);
     }
 
-    // 모든 알림 조회
+    // 모든 알림 조회 - 테스트완료
     @GetMapping("/all")
-    public Flux<NotificationTemplate> getAllNotifications(@RequestHeader Long memberId) {
+    public Flux<NotificationData> getAllNotifications(@RequestHeader Long memberId) {
         return notificationService.getAllNotifications(memberId);
     }
 
-    // 안읽은 알림 개수 받기
+    // 안읽은 알림 개수 받기 - 테스트완료
     @GetMapping("/count")
-    public Mono<Long> getUnreadNotificationCount(@RequestHeader Long memberId) {
+    public Mono<Integer> getUnreadNotificationCount(@RequestHeader Long memberId) {
         return notificationService.countUnreadNotifications(memberId);
     }
 
-    // 개별 알림 읽음처리
+    // 개별 알림 읽음처리 - 테스트완료
     @PutMapping("/{id}/read")
     public Mono<Void> markNotificationAsRead(
             @PathVariable String id, @RequestHeader Long memberId) {
         return notificationService.markNotificationAsRead(id, memberId);
     }
 
-    // 모든 알림 읽음처리
+    // 모든 알림 읽음처리 - 테스트완료
     @PutMapping("/read/all")
     public Mono<Void> markAllNotificationsAsRead(@RequestHeader Long memberId) {
         return notificationService.markAllNotificationsAsRead(memberId);
     }
 
-    // 개별 알림 삭제
+    // 개별 알림 삭제 - 테스트완료
     @DeleteMapping("/{id}")
     public Mono<Void> deleteNotification(
             @PathVariable String id, @RequestHeader Long memberId) {
         return notificationService.deleteNotification(id, memberId);
     }
 
-    // 모든 알림 삭제
+    // 모든 알림 삭제 - 테스트완료
     @DeleteMapping("")
     public Mono<Void> deleteAllNotifications(@RequestHeader Long memberId) {
         return notificationService.deleteAllNotifications(memberId);
     }
 
-    // 구독하기
+    // 구독하기 - 테스트완료 (SQS와 통합한 테스트 필요)
     @GetMapping(value = "/subscription", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<ServerSentEvent<NotificationTemplate>> subscribeToNotifications(@RequestHeader Long memberId) {
+    public Flux<ServerSentEvent<NotificationData>> subscribeToNotifications(@RequestHeader Long memberId) {
         return sseNotificationService.streamNotifications(memberId);
     }
 
