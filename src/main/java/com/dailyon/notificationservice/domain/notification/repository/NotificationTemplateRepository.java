56eea3b0162ebcd1571f4da8f6b904465360dd07
CessnaJ <eon8748@gmail.com>
2023-12-28 16:08:22 +0900

    [CHORE] 불필요한 쿼리 삭제 NotificationTemplateRepository

--- src/main/java/com/dailyon/notificationservice/domain/notification/repository/NotificationTemplateRepository.java
+++ src/main/java/com/dailyon/notificationservice/domain/notification/repository/NotificationTemplateRepository.java
@@ -8,23 +8,9 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Collection;
+
 @Repository
 public interface NotificationTemplateRepository extends ReactiveMongoRepository<NotificationTemplate, String> {
-
-        // 개별 알림 읽음 처리 쿼리
-//        @Query("{'$and': [{'_id': ?0}, {'memberId': ?1}]}")
-//        Mono<Void> setNotificationRead(String notificationId, Long memberId);
-
-        // 모든 알림 읽음 처리 쿼리
-//        @Query("{'memberId': ?0}")
-//        Mono<Void> setAllNotificationsReadForMember(Long memberId);
-
-        // 개별 알림 삭제 쿼리
-//        @Query("{'$and': [{'_id': ?0}, {'memberId': ?1}]}")
-//        Mono<Void> setNotificationDeleted(String notificationId, Long memberId);
-
-        // 모든 알림 삭제 쿼리
-//        @Query("{'memberId': ?0}")
-//        Mono<Void> setAllNotificationsDeletedForMember(Long memberId);
-
+    Flux<NotificationTemplate> findByIdIn(Collection<String> ids);
 }
