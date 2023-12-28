fc014f7436e5cfc29619868e5bf5ae51ab7a73bd
CessnaJ <eon8748@gmail.com>
2023-12-28 16:06:16 +0900

    [FEAT] UserNotification Document의 배열 Set으로 변경, 기본값으로 empty hashSet 할당, method dml위한 method 추가

--- src/main/java/com/dailyon/notificationservice/domain/notification/document/UserNotification.java
+++ src/main/java/com/dailyon/notificationservice/domain/notification/document/UserNotification.java
@@ -10,7 +10,9 @@ import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Builder
@@ -22,15 +24,39 @@ public class UserNotification {
     @Id
     private String id;
 
+    // 알림 연결시 memberId로 찾기 위해 index 추가
     @Indexed
     private Long memberId;
 
     @CreatedDate
     private Instant createdAt;
 
-    private List<String> unread;
+    // NotificationTemplate의 id를 hashSet로 가짐. unread, read, deleted로 관리
+    @Builder.Default private Set<String> unread = new HashSet<>(); // document 생성시 not null 보장
+    @Builder.Default private Set<String> read = new HashSet<>();
+    @Builder.Default private Set<String> deleted = new HashSet<>();
 
-    private List<String> read;
+    public void markAsRead(String notificationId) {
+        unread.remove(notificationId);
+        read.add(notificationId);
+    }
 
-    private List<String> deleted;
+    // unread -> read
+    public void markAsReadAllNotifications() {
+        read.addAll(unread);
+        unread.clear();
+    }
+
+    public void deleteNotification(String notificationId) {
+        unread.remove(notificationId);
+        read.remove(notificationId);
+        deleted.add(notificationId);
+    }
+
+    public void deleteAllNotifications() {
+        deleted.addAll(unread);
+        deleted.addAll(read);
+        unread.clear();
+        read.clear();
+    }
 }
