d54ffed14026498b16105c786b7e6b6dfdd85a8c
CessnaJ <eon8748@gmail.com>
2023-12-28 16:09:09 +0900

    [FEAT] UserNotification 조회 쿼리 추가

--- src/main/java/com/dailyon/notificationservice/domain/notification/repository/UserNotificationRepository.java
+++ src/main/java/com/dailyon/notificationservice/domain/notification/repository/UserNotificationRepository.java
@@ -2,22 +2,31 @@ package com.dailyon.notificationservice.domain.notification.repository;
 
 import com.dailyon.notificationservice.domain.notification.document.NotificationTemplate;
 import com.dailyon.notificationservice.domain.notification.document.UserNotification;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
+@Repository
 public interface UserNotificationRepository extends ReactiveMongoRepository<UserNotification, String> {
 
-    Flux<UserNotification> findTop5ByMemberIdAndDeletedFalseOrderByCreatedAtDesc(Long memberId, Sort sort);
-    Flux<UserNotification> findByMemberIdAndDeletedFalseOrderByCreatedAtDesc(Long memberId, Sort sort);
-    Mono<Long> countByMemberIdAndUnreadNotInAndDeletedFalse(Long memberId, List<String> emptyList);
+    // memberId에 해당하는 UserNotification에서 최근 5개의 unread 알림 ID를 조회
+    @Query(value = "{ 'memberId': ?0, 'deleted': false }",
+            sort = "{ 'createdAt': -1 }",
+            fields = "{ 'unread': 1 }")
+    Flux<UserNotification> findRecentUnreadByMemberId(Long memberId, Pageable pageable);
 
-    //
-//    Mono<Void> setNotificationRead(String notificationId, Long memberId);
-//    Mono<Void> setAllNotificationsReadForMember(Long memberId);
-//    Mono<Void> setNotificationDeleted(String notificationId, Long memberId);
-//    Mono<Void> setAllNotificationsDeletedForMember(Long memberId);
+    // memberId에 해당하는 UserNotification에서 모든 unread, read 알림 ID를 조회
+    @Query(value = "{ 'memberId': ?0, 'deleted': false }",
+            sort = "{ 'createdAt': -1 }",
+            fields = "{ 'unread': 1, 'read': 1 }")
+    Flux<UserNotification> findAllNotificationsByMemberId(Long memberId);
+
+    Mono<UserNotification> findByMemberId(Long memberId);
+
 }
