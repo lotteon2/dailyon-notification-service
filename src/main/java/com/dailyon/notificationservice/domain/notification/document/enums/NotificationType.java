c7b634a1718af56176a11410583f060c685e6bc9
CessnaJ <eon8748@gmail.com>
2023-12-28 16:06:46 +0900

    [FEAT] NotificationType에 ORDER_COMPLETE 추가

--- src/main/java/com/dailyon/notificationservice/domain/notification/document/enums/NotificationType.java
+++ src/main/java/com/dailyon/notificationservice/domain/notification/document/enums/NotificationType.java
@@ -5,8 +5,9 @@ import lombok.Getter;
 @Getter
 public enum NotificationType {
     PRODUCT_RESTOCK("재입고", "상품 재입고 알림."),
+    ORDER_COMPLETE("주문완료", "주문이 완료되었습니다."),
     ORDER_SHIPPED("선적", "주문하신 상품이 출발했습니다.");
-
+    // 정의하면서 넣을 예정
 
     private final String name;
     private final String description;
