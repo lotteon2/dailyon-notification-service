bb464946b0edd2ed2a45ebcdb4976bac42625a1f
CessnaJ <eon8748@gmail.com>
2024-01-06 16:40:46 +0900

    [FEAT] 공통 ErrorResponseException 작성

---
+++ src/main/java/com/dailyon/notificationservice/common/exceptions/ErrorResponseException.java
@@ -0,0 +1,12 @@
+package com.dailyon.notificationservice.common.exceptions;
+
+public class ErrorResponseException extends RuntimeException {
+    private static final String DEFAULT_MESSAGE = "올바르지 않은 요청입니다.";
+    public ErrorResponseException() {
+        super(DEFAULT_MESSAGE);
+    }
+
+    public ErrorResponseException(String message) {
+        super(message);
+    }
+}
