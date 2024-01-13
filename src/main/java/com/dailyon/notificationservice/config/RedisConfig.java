9c5de41f27b1eadedd34f9ec494d502166cb66b0
CessnaJ <eon8748@gmail.com>
2024-01-14 01:04:48 +0900

    [CHORE] 사용하지 않는 코드 임시 주석처리

--- src/main/java/com/dailyon/notificationservice/config/RedisConfig.java
+++ src/main/java/com/dailyon/notificationservice/config/RedisConfig.java
@@ -63,13 +63,13 @@ public class RedisConfig {
     public ReactiveRedisTemplate<String, String> reactiveStringRedisTemplate(
             ReactiveRedisConnectionFactory factory) {
         StringRedisSerializer keySerializer = new StringRedisSerializer();
-        StringRedisSerializer valueSerializer = new StringRedisSerializer();
+//        StringRedisSerializer valueSerializer = new StringRedisSerializer();
 
-        RedisSerializationContext.RedisSerializationContextBuilder<String, String> builder =
-                RedisSerializationContext.newSerializationContext(keySerializer);
-        return new ReactiveStringRedisTemplate(factory, RedisSerializationContext.string());
+//        RedisSerializationContext.RedisSerializationContextBuilder<String, String> builder =
+//                RedisSerializationContext.newSerializationContext(keySerializer);
 //        RedisSerializationContext<String, String> context = builder.value(valueSerializer).build();
 //        return new ReactiveRedisTemplate<>(factory, context);
+        return new ReactiveStringRedisTemplate(factory, RedisSerializationContext.string());
     }
 
 //    @Bean
