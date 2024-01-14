88710530eb23f3f21b096d5eaa46aaea120b6ae6
CessnaJ <eon8748@gmail.com>
2024-01-14 15:26:18 +0900

    [FEAT] process위한 ExtendedNotificationData DTO 작성

---
+++ src/main/java/com/dailyon/notificationservice/domain/notification/dto/ExtendedNotificationData.java
@@ -0,0 +1,28 @@
+package com.dailyon.notificationservice.domain.notification.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExtendedNotificationData {
+    private List<Long> whoToNotify;
+    private NotificationData notificationData;
+    private Map<String, String> parameters;
+    public static ExtendedNotificationData of(List<Long> whoToNotify, NotificationData notificationData, Map<String, String> parameters) {
+        return ExtendedNotificationData.builder()
+                .whoToNotify(whoToNotify)
+                .notificationData(notificationData)
+                .parameters(parameters)
+                .build();
+    }
+}
+
