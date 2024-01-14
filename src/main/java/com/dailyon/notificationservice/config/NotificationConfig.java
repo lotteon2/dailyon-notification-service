c20deb99da45666a4506d5a9071e370b23f0c57d
CessnaJ <eon8748@gmail.com>
2024-01-14 23:28:39 +0900

    [FEAT] NOTIFICATION_STREAM_TTL 추가

--- src/main/java/com/dailyon/notificationservice/config/NotificationConfig.java
+++ src/main/java/com/dailyon/notificationservice/config/NotificationConfig.java
@@ -1,9 +1,7 @@
 package com.dailyon.notificationservice.config;
 
 
-import org.springframework.context.annotation.Configuration;
-
-
 public class NotificationConfig {
     public static final String NOTIFICATIONS_STREAM_KEY = "notifications:stream";
+    public static final Long NOTIFICATION_STREAM_TTL = 5 * 60 * 1000L; // ms단위 5분
 }
